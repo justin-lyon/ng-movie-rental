cbaaf4b8c389c005f5cfa64e744706925a33f0ca
Justin Lyon <jml6487@gmail.com>
2020-11-05 22:07:19 -0600

    run lint on project

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest.base.setup.ts'],
 
-  testMatch: ["<rootDir>/src/**/*.spec.ts"]
-}
+  testMatch: ['<rootDir>/src/**/*.spec.ts']
+};
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class HomeComponent {}
