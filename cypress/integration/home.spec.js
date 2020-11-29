5b5830e70de46c43e8ee4a4e120a9287664a8afb
Justin Lyon <jml6487@gmail.com>
2020-11-28 20:54:19 -0600

    write int test for app

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,4 +6,12 @@ describe('the home page', () => {
   it('should display', () => {
     cy.contains('welcome home');
   });
+
+  it('should have a sidebar and toolbar', () => {
+    cy.get('mat-sidenav').contains('Menu');
+    cy.get('mat-sidenav').contains('Home');
+    cy.get('mat-sidenav').contains('My Account');
+    cy.get('mat-sidenav').contains('Preferences');
+    cy.get('mat-toolbar').contains('Movie Rental');
+  });
 });
