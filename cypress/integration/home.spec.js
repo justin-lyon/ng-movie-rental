fe84824ecdd082ccb00e90719c73ed6d25929884
Justin Lyon <jml6487@gmail.com>
2020-11-28 21:05:39 -0600

    change sidebar to always be a drawer

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -8,10 +8,13 @@ describe('the home page', () => {
   });
 
   it('should have a sidebar and toolbar', () => {
+    cy.get('mat-sidenav').should('not.be.visible');
+    cy.get('mat-toolbar').contains('Movie Rental');
+    cy.get('mat-icon').contains('menu').click();
+    cy.get('mat-sidenav').should('be.visible');
     cy.get('mat-sidenav').contains('Menu');
     cy.get('mat-sidenav').contains('Home');
     cy.get('mat-sidenav').contains('My Account');
     cy.get('mat-sidenav').contains('Preferences');
-    cy.get('mat-toolbar').contains('Movie Rental');
   });
 });
