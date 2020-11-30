60b7e2d42e6124e81eb72b1cc2aec45680a5c2f3
Justin Lyon <jml6487@gmail.com>
2020-11-29 20:56:20 -0600

    add account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
+import { AccountComponent } from './pages/account/account.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'account', component: AccountComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { RouterModule } from '@angular/router';
+
 import { MaterialModule } from '../material.module';
 import { NaviComponent } from './navi/navi.component';
 
 @NgModule({
   declarations: [NaviComponent],
   exports: [MaterialModule, FlexLayoutModule, LayoutModule, NaviComponent],
-  imports: [CommonModule, MaterialModule, FlexLayoutModule, LayoutModule]
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    LayoutModule,
+    RouterModule
+  ]
 })
 export class SharedModule {}
