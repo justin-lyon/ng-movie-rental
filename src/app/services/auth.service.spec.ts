9f1ac150bad0cbdda6616034795624ce21215c75
Justin Lyon <jml6487@gmail.com>
2021-01-15 21:13:52 -0600

    jest stable

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,18 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
+jest.mock('@angular/router');
+jest.mock('@angular/common/http');
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, Router]
+    });
     service = TestBed.inject(AuthService);
   });
 
