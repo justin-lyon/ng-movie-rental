03ad7d8000bc58dada006bd3db26f5e4b0ecf2ad
Justin Lyon <jml6487@gmail.com>
2021-01-15 21:11:20 -0600

    jest.mock('@angular/router'); just works...

--- src/app/pages/signup/signup.component.spec.ts
+++ src/app/pages/signup/signup.component.spec.ts
@@ -1,21 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 import { SignupComponent } from './signup.component';
+import { AuthService } from './../../services/auth.service';
 
-import { MatFormFieldHarness } from '@angular/material/form-field/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+jest.mock('@angular/router');
+jest.mock('./../../services/auth.service');
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
   let loader: HarnessLoader;
+  let router: Router;
+  let authService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,11 +32,14 @@ describe('SignupComponent', () => {
         MatFormFieldModule,
         MatInputModule,
         NoopAnimationsModule
-      ]
+      ],
+      providers: [Router, AuthService]
     }).compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    authService = TestBed.get(AuthService);
     fixture = TestBed.createComponent(SignupComponent);
     loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
