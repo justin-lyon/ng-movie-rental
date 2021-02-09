5f5e1b65f14662c9b1c1c5ebe62d99f4db8cf3f5
Justin Lyon <jml6487@gmail.com>
2021-02-08 21:55:47 -0600

    login page spec

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -9,6 +9,11 @@ import { LoginComponent } from './login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
+const newUser = {
+  email: 'belcher.louise@hotmail.com',
+  password: 'kuchiK0pi!'
+};
+
 jest.mock('@angular/router');
 jest.mock('./../../services/auth.service');
 describe('LoginComponent', () => {
@@ -46,5 +51,72 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInt', () => {
+    it('should init with formGroup with controls', () => {
+      expect(component.formGroup).toBeDefined();
+      const { email, password } = component.formGroup.value;
+      expect(email).toBeDefined();
+      expect(password).toBeDefined();
+    });
+
+    it('should require email', () => {
+      const email = component.formGroup.controls.email;
+      email.markAsTouched();
+      expect(email.invalid).toBeTruthy();
+      expect(email.errors.required).toBeTruthy();
+    });
+
+    it('should validate email address', () => {
+      const email = component.formGroup.controls.email;
+      email.setValue('not-tinas-email');
+      expect(email.invalid).toBeTruthy();
+      expect(email.errors.email).toBeTruthy();
+    });
+
+    it('should require password', () => {
+      const password = component.formGroup.controls.password;
+      password.markAsTouched();
+      expect(password.invalid).toBeTruthy();
+      expect(password.errors.required).toBeTruthy();
+    });
+  });
+
+  describe('login', () => {
+    it('should login and go to home page', async () => {
+      const { email, password } = component.formGroup.controls;
+      email.setValue(newUser.email);
+      password.setValue(newUser.password);
+
+      await component.login();
+
+      expect(authService.login).toHaveBeenCalledTimes(1);
+      expect(authService.login).toHaveBeenCalledWith(
+        newUser.email,
+        newUser.password
+      );
+      expect(router.navigate).toHaveBeenCalledTimes(1);
+      expect(router.navigate).toHaveBeenCalledWith(['home']);
+    });
+
+    it('should log an error if there is a problem logging in', async () => {
+      authService.login = jest.fn().mockRejectedValue(undefined);
+      console.error = jest.fn();
+
+      const { email, password } = component.formGroup.controls;
+      email.setValue(newUser.email);
+      password.setValue(newUser.password);
+
+      await component.login();
+
+      expect(authService.login).toHaveBeenCalledTimes(1);
+      expect(authService.login).toHaveBeenCalledWith(
+        newUser.email,
+        newUser.password
+      );
+      expect(router.navigate).toHaveBeenCalledTimes(0);
+      expect(console.error).toHaveBeenCalledTimes(1);
+    });
   });
 });
