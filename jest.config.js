ac5d4cddaf48352854f4a7bb78943671d8cb7a46
Justin Lyon <jml6487@gmail.com>
2021-02-12 21:57:23 -0600

    rm noop interceptor

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest.base.setup.ts'],
-
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/src/main.ts',
+    '/src/polyfills.ts',
+    '/models/',
+    '/environments/',
+    '/*.module.ts'
+  ],
   testMatch: ['<rootDir>/src/**/*.spec.ts']
 };
