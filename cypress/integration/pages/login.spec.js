00b8b81f23c4876c0d436453894f8e1e28331784
Justin Lyon <jml6487@gmail.com>
2021-02-14 21:38:14 -0600

    finish cypress tests for login

---
+++ cypress/integration/pages/login.spec.js
@@ -0,0 +1,72 @@
+context('login page', () => {
+  const { logout, typeValue, assertMatError } = cy;
+  const container = 'div.container';
+  const email = 'input[formcontrolname="email"]';
+  const password = 'input[type="password"][formcontrolname="password"]';
+  const cancelBtn = 'button:contains("Cancel")';
+  const loginBtn = 'button:contains("Login")';
+
+  beforeEach(() => {
+    logout();
+    cy.visit('/login');
+  });
+
+  describe('the form', () => {
+    it('should have two fields and two buttons', () => {
+      cy.get(container).within(() => {
+        cy.get(email).should('exist');
+        cy.get(password).should('exist');
+        cy.get(cancelBtn).should('be.enabled');
+        cy.get(loginBtn).should('be.disabled');
+      });
+    });
+
+    it('should validate the form', () => {
+      cy.get(container).within(() => {
+        cy.get(loginBtn).should('be.disabled');
+        cy.get(email).focus().blur();
+        assertMatError('This field is required.');
+        typeValue(email, 'not-an-email');
+        assertMatError('Please enter a valid email address.');
+        typeValue(email, 'burgerbob@burgerfans.net');
+        cy.get('mat-error').should('not.exist');
+
+        cy.get(loginBtn).should('be.disabled');
+        cy.get(password).focus().blur();
+        assertMatError('This field is required.');
+        typeValue(password, 'any thing at all');
+        cy.get('mat-error').should('not.exist');
+
+        cy.get(loginBtn).should('be.enabled');
+      });
+    });
+  });
+
+  describe('submit async validation failure', () => {
+    beforeEach(() => {
+      cy.intercept('POST', '/login', { statusCode: 403 });
+    });
+
+    it('should report invalid username or password on error', () => {
+      cy.get(container).within(() => {
+        typeValue(email, 'burgerbob@burgerfans.net');
+        typeValue(password, 'noHumanMe@t123');
+        cy.get(loginBtn).click();
+        assertMatError('Invalid username or password.');
+      });
+    });
+  });
+
+  describe('submit async validation success', () => {
+    beforeEach(() => {
+      cy.intercept('POST', '/login', { fixture: 'login.txt' });
+    });
+
+    it('should navigate to /home on success', () => {
+      typeValue(email, 'burgerbob@burgerfans.net');
+      typeValue(password, 'noHumanMe@t123');
+      cy.get(loginBtn).click();
+      cy.url().should('include', '/home');
+    });
+  });
+});
