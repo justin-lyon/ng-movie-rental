13ce3e6b9c037215796f45fc11729bcdc86cabae
Justin Lyon <jml6487@gmail.com>
2021-03-11 21:20:37 -0600

    stabilize search component spec

--- src/app/pages/search/search.component.spec.ts
+++ src/app/pages/search/search.component.spec.ts
@@ -1,18 +1,63 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
+import { MovieView } from './../../models/movie.view';
+import { SearchService } from './../../services/search.service';
 import { SearchComponent } from './search.component';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+@Component({
+  selector: 'app-header',
+  template: ''
+})
+class MockHeaderComponent {
+  @Input()
+  title: string;
+  @Input()
+  subtitle: string;
+}
+
+@Component({
+  selector: 'app-movie-grid',
+  template: ''
+})
+class MockMovieGrid {
+  @Input()
+  movies: MovieView[];
+}
+
+const term = 'the goonies';
+const MockActivatedRoute = {
+  provide: ActivatedRoute,
+  useValue: {
+    queryParams: of({ term })
+  }
+};
+
+// jest.mock('@angular/router');
+jest.mock('./../../services/search.service');
 describe('SearchComponent', () => {
+  let route: ActivatedRoute;
+  let searchService: SearchService;
+
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SearchComponent]
+      declarations: [SearchComponent, MockHeaderComponent, MockMovieGrid],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [SearchService, MockActivatedRoute]
     }).compileComponents();
   });
 
   beforeEach(() => {
+    route = TestBed.inject(ActivatedRoute);
+    searchService = TestBed.inject(SearchService);
+
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
