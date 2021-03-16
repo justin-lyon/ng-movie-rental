98dce94ab6732b9e6633ec01b9580faa06d5066d
Justin Lyon <jml6487@gmail.com>
2021-03-15 21:32:38 -0500

    moviegrid spec creates

--- src/app/shared/movie-grid/movie-grid.component.spec.ts
+++ src/app/shared/movie-grid/movie-grid.component.spec.ts
@@ -1,6 +1,17 @@
+import { MovieView } from './../../models/movie.view';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieGridComponent } from './movie-grid.component';
+
+@Component({
+  selector: 'app-movie-card',
+  template: ''
+})
+class MockMovieCard {
+  @Input()
+  movie: MovieView;
+}
 
 describe('MovieGridComponent', () => {
   let component: MovieGridComponent;
@@ -8,7 +19,7 @@ describe('MovieGridComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MovieGridComponent]
+      declarations: [MovieGridComponent, MockMovieCard]
     }).compileComponents();
   });
 
