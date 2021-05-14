b7ae2306cb454648bd9a844c8ffd2fd57225f45f
Justin Lyon <jml6487@gmail.com>
2021-05-13 21:28:11 -0500

    fix failing cypress spec

--- cypress/integration/pages/signup.spec.js
+++ cypress/integration/pages/signup.spec.js
@@ -24,6 +24,7 @@ const messages = {
 const interceptSignup = () => {
   cy.intercept('POST', '/signup', { fixture: 'signup.json' });
   cy.intercept('POST', '/login', { fixture: 'login.txt' });
+  cy.intercept('GET', '/movies', []);
 };
 
 // Custom Cypress Commands
@@ -50,74 +51,85 @@ context('the signup page', () => {
     });
   });
 
-  it('should validate the form', () => {
-    cy.get(container).within(() => {
-      // Username
-      cy.get(submitBtn).should('be.disabled');
-      cy.get(username).focus().blur();
-      assertMatError(messages.required);
-      typeValue(username, 't');
-      assertMatError(messages.minLength2);
-      typeValue(username, '123456789123456789123456789');
-      assertMatError(messages.maxLength20);
-      typeValue(username, 'turkeylover');
-      cy.get('mat-error').should('not.exist');
+  describe('form validation', () => {
+    it('should validate username', () => {
+      cy.get(container).within(() => {
+        cy.get(submitBtn).should('be.disabled');
+        cy.get(username).focus().blur();
+        assertMatError(messages.required);
+        typeValue(username, 't');
+        assertMatError(messages.minLength2);
+        typeValue(username, '123456789123456789123456789');
+        assertMatError(messages.maxLength20);
+        typeValue(username, 'turkeylover');
+        cy.get('mat-error').should('not.exist');
+      });
+    });
 
-      // Email
-      cy.get(submitBtn).should('be.disabled');
-      cy.get(email).focus().blur();
-      assertMatError(messages.required);
-      typeValue(email, 'notanemail');
-      assertMatError(messages.email);
-      typeValue(email, 'bobsburgers@hotmail.com');
-      cy.get('mat-error').should('not.exist');
+    it('should validate email', () => {
+      cy.get(container).within(() => {
+        cy.get(submitBtn).should('be.disabled');
+        cy.get(email).focus().blur();
+        assertMatError(messages.required);
+        typeValue(email, 'notanemail');
+        assertMatError(messages.email);
+        typeValue(email, 'bobsburgers@hotmail.com');
+        cy.get('mat-error').should('not.exist');
+      });
+    });
 
-      // Password
-      cy.get(submitBtn).should('be.disabled');
-      cy.get(password).focus().blur();
-      assertMatError(messages.required);
-      typeValue(password, '1');
-      assertMatError(messages.minLength8);
-      typeValue(password, '12345678');
-      assertMatError(messages.lowercase);
-      typeValue(password, 'abcd1234');
-      assertMatError(messages.uppercase);
-      typeValue(password, 'Abcdefgh');
-      assertMatError(messages.number);
-      typeValue(password, 'Abcd1234');
-      assertMatError(messages.special);
-      typeValue(password, 'Abc1234!');
-      cy.get('mat-error').should('not.exist');
+    it('should validate password', () => {
+      cy.get(container).within(() => {
+        cy.get(submitBtn).should('be.disabled');
+        cy.get(password).focus().blur();
+        assertMatError(messages.required);
+        typeValue(password, '1');
+        assertMatError(messages.minLength8);
+        typeValue(password, '12345678');
+        assertMatError(messages.lowercase);
+        typeValue(password, 'abcd1234');
+        assertMatError(messages.uppercase);
+        typeValue(password, 'Abcdefgh');
+        assertMatError(messages.number);
+        typeValue(password, 'Abcd1234');
+        assertMatError(messages.special);
+        typeValue(password, 'Abc1234!');
+        cy.get('mat-error').should('not.exist');
+      });
+    });
 
-      // Password2
-      cy.get(submitBtn).should('be.disabled');
-      cy.get(password2).focus().blur();
-      assertMatError(messages.required);
-      typeValue(password2, 'not a match');
-      assertMatError(messages.matching);
-      typeValue(password2, 'Abc1234!');
-      cy.get('mat-error').should('not.exist');
-
+    it('should validate password2', () => {
+      cy.get(container).within(() => {
+        cy.get(submitBtn).should('be.disabled');
+        cy.get(password2).focus().blur();
+        assertMatError(messages.required);
+        typeValue(password2, 'not a match');
+        assertMatError(messages.matching);
+        typeValue(password2, 'Abc1234!');
+        cy.get('mat-error').should('not.exist');
+      });
       cy.get(submitBtn).should('be.enabled');
     });
   });
 
-  it('should navigate to Home on submit', () => {
-    interceptSignup();
+  describe('form submit', () => {
+    it('should navigate to home on submit', () => {
+      interceptSignup();
 
-    cy.get(container).within(() => {
-      typeValue(username, 'turkeylover');
-      typeValue(email, 'bobsburgers@hotmail.com');
-      typeValue(password, 'N0HumanM3@t');
-      typeValue(password2, 'N0HumanM3@t');
+      cy.get(container).within(() => {
+        typeValue(username, 'turkeylover');
+        typeValue(email, 'bobsburgers@hotmail.com');
+        typeValue(password, 'N0HumanM3@t');
+        typeValue(password2, 'N0HumanM3@t');
 
-      cy.get(submitBtn).click();
-      cy.url().should('include', '/home');
-    });
+        cy.get(submitBtn).click();
+        cy.url().should('include', '/home');
+      });
 
-    cy.fixture('login.txt').then(data => {
-      const token = data.split(' ')[1];
-      expect(localStorage.getItem('token')).to.eq(token);
+      cy.fixture('login.txt').then(data => {
+        const token = data.split(' ')[1];
+        expect(localStorage.getItem('token')).to.eq(token);
+      });
     });
   });
 });
