d2c3da254c9035516e3188af6b19bef721a6f8a1
Justin Lyon <jml6487@gmail.com>
2021-05-18 22:23:26 -0500

    init movie detail cy spec

---
+++ cypress/integration/pages/movie-detail.js
@@ -0,0 +1,9 @@
+context('movie-detail', () => {
+  beforeEach(() => {});
+
+  it('should display', () => {});
+
+  describe('fields', () => {
+    it('should display field A', () => {});
+  });
+});
